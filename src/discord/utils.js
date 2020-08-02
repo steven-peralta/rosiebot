81b51c01ecbc64d53bc38bdca725f842cbea1ed8
steven-peralta <steven@stevenperalta.dev>
2020-08-02 06:04:34 -0500

    add travis.yml

---
+++ src/discord/utils.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getId = void 0;
+exports.getId = (userId, serverId) => userId + (serverId !== null && serverId !== void 0 ? serverId : '');
