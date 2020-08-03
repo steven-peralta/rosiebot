d8f4f25927252a1015d7b36e448882253710bd3b
steven-peralta <steven@stevenperalta.dev>
2020-08-02 19:55:31 -0500

    fix(dep): remove dist among other things from the repo pt.2

--- dist/discord/utils.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getId = void 0;
-exports.getId = (userId, serverId) => userId + (serverId !== null && serverId !== void 0 ? serverId : '');
-//# sourceMappingURL=utils.js.map
